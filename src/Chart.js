aafebb55ca18882b66ea0bc8a82f4732481ebbb8
sanja <sanja712@yahoo.com>
2022-04-11 13:58:46 +0200

    zevs commit

--- src/Chart.js
+++ src/Chart.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 
 import {
   Chart as ChartJS,
@@ -24,10 +24,18 @@ ChartJS.register(
 );
 
 export default function ChartTest() {
-  const [dataChart, setDataChart] = useState({
-    label: [],
-    data: []
-  });
+  const [dataChartVoice, setDataVoice] = useState({label: [], data: []});
+  const [dataChartLte, setDataLte] = useState({label: [], data: []});
+  const [dataChartSRVCC, setDataSRVCC] = useState({label: [], data: []});
+  const [dataChartDrop, setDataDrop] = useState({label: [], data: []});
+  const [dataChartSucc23, setDataSucc23] = useState({label: [], data: []});
+  const [dataChartSucc34, setDataSucc34] = useState({label: [], data: []});
+  const [dataChartDrop23, setDataDrop23] = useState({label: [], data: []});
+  const [dataChartDrop34, setDataDrop34] = useState({label: [], data: []});
+  const [dataChartThroup45, setDataThroup45] = useState({label: [], data: []});
+  const [dataChartThroup1, setDataThroup1] = useState({label: [], data: []});
+  const [dataChartLteavg, setDataLteavg] = useState({label: [], data: []});
+  const [dataChartSuccall, setDataSuccall] = useState({label: [], data: []});
   const [Chartloading, setChartLoading] = useState(false);
 
   useEffect(() => {
@@ -37,31 +45,95 @@ export default function ChartTest() {
         const data = await response.json()
         const requiredDataFromResponse = data.body;
         const dates: any[] = [];
-        const erab: any[] = [];
+        const voice: any[] = [];
+        const lte: any[] = [];
+        const srvcc: any[] = [];
+        const drop: any[] = [];
+        const drop23: any[] = [];
+        const drop34: any[] = [];
+        const succ23: any[] = [];
+        const succ34: any[] = [];
+        const succall: any[] = [];
+        const throup1: any[] = [];
+        const throup45: any[] = [];
+        const lteavg: any[] = [];
         requiredDataFromResponse.forEach(eachSensorItem => {
             dates.push(eachSensorItem.date)
-            erab.push(parseFloat(eachSensorItem["VoLTE E-RAB (QCI1) Success Rate (Voice) (%)"]).toFixed(2))
+            voice.push(parseFloat(eachSensorItem["VoLTE E-RAB (QCI1) Success Rate (Voice) (%)"]).toFixed(2))
+            lte.push(parseFloat(eachSensorItem["VOLTE Handover Success Rate (Intra-LTE)"]).toFixed(2))
+            srvcc.push(parseFloat(eachSensorItem["VOLTE SRVCC Handover Success Rate"]).toFixed(2))
+            drop.push(parseFloat(eachSensorItem["VoLTE Call Drop Rate"]).toFixed(2))
+            succ34.push(parseFloat(eachSensorItem["Data Service Setup Success Rate (3G/4G)"]).toFixed(2))
+            succ23.push(parseFloat(eachSensorItem["Voice Call Setup Success Rate (2G/3G)"]).toFixed(2))
+            drop23.push(parseFloat(eachSensorItem["Voice Call Drop Rate (2G/3G)"]).toFixed(2))
+            drop34.push(parseFloat(eachSensorItem["Data Service Drop Rate (3G/4G)"]).toFixed(2))
+            throup45.push(parseFloat(eachSensorItem["% cells < 4.5 Mbps DL user Throughput (4G)"]).toFixed(2))
+            throup1.push(parseFloat(eachSensorItem["% cells < 1 Mbps UL user Throughput (4G)"]).toFixed(2))
+            lteavg.push(parseFloat(eachSensorItem["LTE Radio Quality Average CQI (%Cells > CQI8)"]).toFixed(2))
+            succall.push(parseFloat(eachSensorItem["Handover Success Rate (All Tech)"]).toFixed(2))
         })
         console.log(dates)
-        console.log(erab)
+        console.log(voice)
 
-        setDataChart({
+        setDataVoice({
             label: dates,
-            data: erab,
+            data: voice,
+        });
+        setDataLte({
+            label: dates,
+            data: lte,
+        });
+        setDataSRVCC({
+            label: dates,
+            data: srvcc,
+        });
+        setDataDrop({
+            label: dates,
+            data: drop,
+        });
+        setDataSucc34({
+            label: dates,
+            data: succ34,
+        });
+        setDataSucc23({
+            label: dates,
+            data: succ23,
+        });
+        setDataDrop23({
+            label: dates,
+            data: drop23,
+        });
+        setDataDrop34({
+            label: dates,
+            data: drop34,
+        });
+        setDataThroup45({
+            label: dates,
+            data: throup45,
+        });
+        setDataThroup1({
+            label: dates,
+            data: throup1,
+        });
+        setDataLteavg({
+            label: dates,
+            data: lteavg,
+        });
+        setDataSuccall({
+            label: dates,
+            data: succall,
         });
         setChartLoading(true)
         }
        fetchData();
       }, [])
 
-  console.log(Chartloading, dataChart)
-
-    const data = {
-    labels: dataChart.label,
+    const dataVoice = {
+    labels: dataChartVoice.label,
     datasets: [
       {
         label: 'VoLTE E-RAB (QCI1) Success Rate (Voice) (%)',
-        data: dataChart.data,
+        data: dataChartVoice.data,
         fill: false,
         backgroundColor: 'rgb(255, 99, 132)',
         borderColor: 'rgba(255, 99, 132, 0.2)',
@@ -69,9 +141,164 @@ export default function ChartTest() {
       }
     ],
   };
-      return(
-        <div style={{ height:'500px',width:'1000px',margin:'0 auto' }}>
-           <Line data={data} />
+    const dataLte = {
+    labels: dataChartLte.label,
+    datasets: [
+      {
+        label: "VOLTE Handover Success Rate (Intra-LTE)",
+        data: dataChartLte.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataSRVCC = {
+    labels: dataChartSRVCC.label,
+    datasets: [
+      {
+        label: "VOLTE SRVCC Handover Success Rate",
+        data: dataChartSRVCC.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataDrop = {
+    labels: dataChartDrop.label,
+    datasets: [
+      {
+        label: "VoLTE Call Drop Rate",
+        data: dataChartDrop.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataSucc34 = {
+    labels: dataChartSucc34.label,
+    datasets: [
+      {
+        label: "Data Service Setup Success Rate (3G/4G)",
+        data: dataChartSucc34.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataSucc23 = {
+    labels: dataChartSucc23.label,
+    datasets: [
+      {
+        label: "Voice Call Setup Success Rate (2G/3G)",
+        data: dataChartSucc23.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataDrop23 = {
+    labels: dataChartDrop23.label,
+    datasets: [
+      {
+        label: "Voice Call Drop Rate (2G/3G)",
+        data: dataChartDrop23.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataDrop34 = {
+    labels: dataChartDrop34.label,
+    datasets: [
+      {
+        label: "Data Service Drop Rate (3G/4G)",
+        data: dataChartDrop34.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataThroup45 = {
+    labels: dataChartThroup45.label,
+    datasets: [
+      {
+        label: "% cells < 4.5 Mbps DL user Throughput (4G)",
+        data: dataChartThroup45.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataThroup1 = {
+    labels: dataChartThroup1.label,
+    datasets: [
+      {
+        label: "% cells < 1 Mbps UL user Throughput (4G)",
+        data: dataChartThroup1.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataLteavg = {
+    labels: dataChartLteavg.label,
+    datasets: [
+      {
+        label: "LTE Radio Quality Average CQI (%Cells > CQI8)",
+        data: dataChartLteavg.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataSuccall = {
+    labels: dataChartSuccall.label,
+    datasets: [
+      {
+        label: "Handover Success Rate (All Tech)",
+        data: dataChartSuccall.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+
+      return (
+        <div style= {{ height:'200px', width:'80%', margin:'0 auto' }} >
+            <Line data={dataVoice} />
+            <Line data={dataLte} />
+            <Line data={dataSRVCC} />
+            <Line data={dataDrop} />
+            <Line data={dataSucc34} />
+            <Line data={dataSucc23} />
+            <Line data={dataDrop23} />
+            <Line data={dataDrop34} />
+            <Line data={dataThroup45} />
+            <Line data={dataThroup1} />
+            <Line data={dataLteavg} />
+            <Line data={dataSuccall} />
         </div>
-      )
+      );
     }
--- src/KPI.js
+++ src/KPI.js
@@ -27,13 +27,19 @@ class KPI extends Component {
     constructor(props) {
         super(props);
         this.state={
-            chartData:{
-                labels: [],
-                datasets: [{
-                    label: "Jos jedan test",
-                    data: [],
-            }]
-        }}
+            dataChartVoice:{label: [], data: [], },
+            dataChartLte:{label: [], data: [], },
+            dataChartSRVCC:{label: [], data: [], },
+            dataChartDrop:{label: [], data: [], },
+            dataChartSucc23:{label: [], data: [], },
+            dataChartSucc34:{label: [], data: [], },
+            dataChartDrop23:{label: [], data: [], },
+            dataChartDrop34:{label: [], data: [], },
+            dataChartThroup45:{label: [], data: [], },
+            dataChartThroup1:{label: [], data: [], },
+            dataChartLteavg:{label: [], data: [], },
+            dataChartSuccall:{label: [], data: [], },
+        }
     }
 
     componentDidMount() {
@@ -46,21 +52,86 @@ class KPI extends Component {
         .then(res => res.json())
         .then((data) => {
             const requiredDataFromResponse = data.body;
-            let dates = []
-            let erab = []
+            const dates: any[] = [];
+            const voice: any[] = [];
+            const lte: any[] = [];
+            const srvcc: any[] = [];
+            const drop: any[] = [];
+            const drop23: any[] = [];
+            const drop34: any[] = [];
+            const succ23: any[] = [];
+            const succ34: any[] = [];
+            const succall: any[] = [];
+            const throup1: any[] = [];
+            const throup45: any[] = [];
+            const lteavg: any[] = [];
             requiredDataFromResponse.forEach(eachSensorItem => {
                 dates.push(eachSensorItem.date)
-                erab.push(parseFloat(eachSensorItem["VoLTE E-RAB (QCI1) Success Rate (Voice) (%)"]).toFixed(2))
+                voice.push(parseFloat(eachSensorItem["VoLTE E-RAB (QCI1) Success Rate (Voice) (%)"]).toFixed(2))
+                lte.push(parseFloat(eachSensorItem["VOLTE Handover Success Rate (Intra-LTE)"]).toFixed(2))
+                srvcc.push(parseFloat(eachSensorItem["VOLTE SRVCC Handover Success Rate"]).toFixed(2))
+                drop.push(parseFloat(eachSensorItem["VoLTE Call Drop Rate"]).toFixed(2))
+                succ34.push(parseFloat(eachSensorItem["Data Service Setup Success Rate (3G/4G)"]).toFixed(2))
+                succ23.push(parseFloat(eachSensorItem["Voice Call Setup Success Rate (2G/3G)"]).toFixed(2))
+                drop23.push(parseFloat(eachSensorItem["Voice Call Drop Rate (2G/3G)"]).toFixed(2))
+                drop34.push(parseFloat(eachSensorItem["Data Service Drop Rate (3G/4G)"]).toFixed(2))
+                throup45.push(parseFloat(eachSensorItem["% cells < 4.5 Mbps DL user Throughput (4G)"]).toFixed(2))
+                throup1.push(parseFloat(eachSensorItem["% cells < 1 Mbps UL user Throughput (4G)"]).toFixed(2))
+                lteavg.push(parseFloat(eachSensorItem["LTE Radio Quality Average CQI (%Cells > CQI8)"]).toFixed(2))
+                succall.push(parseFloat(eachSensorItem["Handover Success Rate (All Tech)"]).toFixed(2))
             })
 
             this.setState({
-                chartData:{
-                    labels: dates,
-                    datasets: [{
-                        label: "Jos jedan test",
-                        data: erab,
-            }]
-        }})
+                dataChartVoice:{
+                    label: dates,
+                    data: voice,
+                },
+                dataChartLte:{
+                    label: dates,
+                    data: lte,
+                },
+                dataChartSRVCC:{
+                    label: dates,
+                    data: srvcc,
+                },
+                dataChartDrop:{
+                    label: dates,
+                    data: drop,
+                },
+                dataChartSucc34:{
+                    label: dates,
+                    data: succ34,
+                },
+                dataChartSucc23:{
+                    label: dates,
+                    data: succ23,
+                },
+                dataChartDrop23:{
+                    label: dates,
+                    data: drop23,
+                },
+                dataChartDrop34:{
+                    label: dates,
+                    data: drop34,
+                },
+                dataChartThroup45:{
+                    label: dates,
+                    data: throup45,
+                },
+                dataChartThroup1:{
+                    label: dates,
+                    data: throup1,
+                },
+                dataChartLteavg:{
+                    label: dates,
+                    data: lteavg,
+                },
+                dataChartSuccall:{
+                    label: dates,
+                    data: succall,
+                },
+
+            })
 
             .catch((error) => {
               console.error('Error:', error);
@@ -68,10 +139,174 @@ class KPI extends Component {
     });
     }
 
+
     render() {
-        return (
-            <p> {this.state.chartData.datasets[0].label} </p>
-        )
+                const dataVoice = {
+    labels: this.state.dataChartVoice.label,
+    datasets: [
+      {
+        label: 'VoLTE E-RAB (QCI1) Success Rate (Voice) (%)',
+        data: this.state.dataChartVoice.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataLte = {
+    labels: this.state.dataChartLte.label,
+    datasets: [
+      {
+        label: "VOLTE Handover Success Rate (Intra-LTE)",
+        data: this.state.dataChartLte.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataSRVCC = {
+    labels: this.state.dataChartSRVCC.label,
+    datasets: [
+      {
+        label: "VOLTE SRVCC Handover Success Rate",
+        data: this.state.dataChartSRVCC.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataDrop = {
+    labels: this.state.dataChartDrop.label,
+    datasets: [
+      {
+        label: "VoLTE Call Drop Rate",
+        data: this.state.dataChartDrop.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataSucc34 = {
+    labels: this.state.dataChartSucc34.label,
+    datasets: [
+      {
+        label: "Data Service Setup Success Rate (3G/4G)",
+        data: this.state.dataChartSucc34.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataSucc23 = {
+    labels: this.state.dataChartSucc23.label,
+    datasets: [
+      {
+        label: "Voice Call Setup Success Rate (2G/3G)",
+        data: this.state.dataChartSucc23.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataDrop23 = {
+    labels: this.state.dataChartDrop23.label,
+    datasets: [
+      {
+        label: "Voice Call Drop Rate (2G/3G)",
+        data: this.state.dataChartDrop23.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataDrop34 = {
+    labels: this.state.dataChartDrop34.label,
+    datasets: [
+      {
+        label: "Data Service Drop Rate (3G/4G)",
+        data: this.state.dataChartDrop34.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataThroup45 = {
+    labels: this.state.dataChartThroup45.label,
+    datasets: [
+      {
+        label: "% cells < 4.5 Mbps DL user Throughput (4G)",
+        data: this.state.dataChartThroup45.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataThroup1 = {
+    labels: this.state.dataChartThroup1.label,
+    datasets: [
+      {
+        label: "% cells < 1 Mbps UL user Throughput (4G)",
+        data: this.state.dataChartThroup1.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataLteavg = {
+    labels: this.state.dataChartLteavg.label,
+    datasets: [
+      {
+        label: "LTE Radio Quality Average CQI (%Cells > CQI8)",
+        data: this.state.dataChartLteavg.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+    const dataSuccall = {
+    labels: this.state.dataChartSuccall.label,
+    datasets: [
+      {
+        label: "Handover Success Rate (All Tech)",
+        data: this.state.dataChartSuccall.data,
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+        yAxisID: 'y-axis-1',
+      }
+    ],
+  };
+
+
+        return ([
+            <div style= {{ height:'200px', width:'80%', margin:'0 auto' }} >
+            <Line data={dataVoice} />
+            </div>,
+            <div style= {{ height:'200px', width:'80%', margin:'0 auto' }} >
+            <Line data={dataDrop} />
+            </div>,
+        ])
     }
 
 }
